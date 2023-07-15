fe9e3b3f818240ed4660bc74828dfd497c43362f
Julian Loiacono <jcloiacon@gmail.com>
2023-07-15 12:26:10 -0400

    Update Banner.js

--- frontend/src/components/Banner.js
+++ frontend/src/components/Banner.js
@@ -3,7 +3,7 @@
 function Banner() {
   return (
     <div className="banner">
-      <h1 className="banner-text">OPENRAD AI - Joe, Tom, Julian</h1>
+      <h1 className="banner-text">OPENRAD AI - Mark, Joe, Tom, Julian</h1>
     </div>
   );
 }
